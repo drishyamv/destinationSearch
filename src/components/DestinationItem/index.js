02e57e4dbe77991288db9e8119d2fdc93e3a4645
drishyamv <126556364+drishyamv@users.noreply.github.com>
2023-04-13 15:08:44 +0530

    Update index.js

--- src/components/DestinationItem/index.js
+++ src/components/DestinationItem/index.js
@@ -1,15 +1,19 @@
+import {Component} from 'react'
+
 import './index.css'
 
-const DestinationItem = props => {
-  const {destinationsList} = props
-  const {imgUrl, name} = destinationsList
+class DestinationItem extends Component {
+  render() {
+    const {destinationsList} = this.props
+    const {imgUrl, name} = destinationsList
 
-  return (
-    <li className="destination-card-container">
-      <img src={imgUrl} className="img-style" alt={name} />
-      <p className="destination-name">{name}</p>
-    </li>
+    return (
+      <li className="destination-card-container">
+        <img src={imgUrl} className="img-style" alt={name} />
+        <p className="destination-name">{name}</p>
+      </li>
   )
+}
 }
 
 export default DestinationItem
